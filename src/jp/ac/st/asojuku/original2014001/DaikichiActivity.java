edf2cf5e798006bb05ed828932f3230e98947656
student <student@PC633S27.fgakusei.local>
2014-05-29 11:49:56 +0900

    5/29

---
+++ src/jp/ac/st/asojuku/original2014001/DaikichiActivity.java
@@ -0,0 +1,15 @@
+package jp.ac.st.asojuku.original2014001;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DaikichiActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.daikichiactivity);
+	}
+
+}
